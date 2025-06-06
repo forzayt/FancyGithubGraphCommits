2bae98ea2ca2ef525c85c760e6ce1f5b31a43b67
vishnu1100 <vishnusanthosh.india@gmail.com>
2025-06-06 13:06:39 +0530

    Refactor commit generation logic to include user input for commit count

--- Commit.js
+++ Commit.js
@@ -2,40 +2,66 @@ import jsonfile from "jsonfile";
 import moment from "moment";
 import simpleGit from "simple-git";
 import random from "random";
+import readline from "readline";
 
 const path = "./data.json";
 const git = simpleGit();
 
-const makeCommits = async (n) => {
-  if (n === 0) {
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const askCommitCount = () => {
+  rl.question("How many commits do you want to make? (Default: 100): ", async (input) => {
+    const count = parseInt(input, 10) || 100;
+    await makeCommits(count);
+    rl.close();
+  });
+};
+
+const makeCommits = async (totalCommits) => {
+  if (totalCommits <= 0) {
     console.log("Done committing, pushing...");
     await git.push();
     return;
   }
 
-  const x = random.int(0, 54); // 0 to 54 weeks
-  const y = random.int(0, 6);  // 0 to 6 days
+  const x = random.int(0, 54); // weeks
+  const y = random.int(0, 6);  // days
 
-  const date = moment()
+  const baseDate = moment()
     .subtract(1, "y")
     .add(1, "d")
     .add(x, "w")
-    .add(y, "d")
-    .format();
+    .add(y, "d");
 
-  const data = { date };
+  const commitsToday = random.int(1, 5); // Do 1 to 5 commits per day
 
-  console.log(`Commit #${101 - n} on ${date}`);
+  for (let i = 0; i < commitsToday && totalCommits > 0; i++) {
+    const date = baseDate.clone().add(i, 's').format(); // Add a few seconds to each
+    const data = { date };
 
-  jsonfile.writeFile(path, data, async () => {
-    try {
-      await git.add([path]);
-      await git.commit(date, undefined, { "--date": date });
-      makeCommits(n - 1);
-    } catch (err) {
-      console.error("Git error:", err);
-    }
-  });
+    console.log(`Commit #${totalCommits} on ${date}`);
+
+    await new Promise((res, rej) => {
+      jsonfile.writeFile(path, data, async () => {
+        try {
+          await git.add([path]);
+          await git.commit(date, undefined, { "--date": date });
+          res();
+        } catch (err) {
+          console.error("Git error:", err);
+          rej(err);
+        }
+      });
+    });
+
+    totalCommits--;
+  }
+
+  // Recursively continue for the next day
+  await makeCommits(totalCommits);
 };
 
-makeCommits(100);
+askCommitCount();
